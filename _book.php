fb693d6e2bde279ec5a6f2f460c96022709c8426
Tera Byte <tubagus.aom.swk@gmail.com>
2024-09-19 13:21:07 +0700

    mergenewbook

--- _book.php
+++ _book.php
@@ -1,6 +1,108 @@
 <h4 class="fs-6 fw-bolder my-4 mb-3">Top Books  <a class="float-end text-primary text-decoration-underline" href="">
     <!-- <small class="fs-8">View All</small> -->
 </a></h4>
+
+                <div class="col-md-4 mb-3">
+                    <a href="single.php">
+                        <div class="app-cover p-2 shadow-md bg-white">
+                            <div class="row">
+                                <div class="img-cover pe-0 col-4"> <img class="rounded" src="assets/images/books/awbook5.jpg" alt="terabytee"></div>
+                                <div class="det mt-2 col-8">
+                                    <h5 class="mb-0 fs-6">Dinamika Industri Hulu</h5>
+                                    <span class="fs-8">Industri</span>
+                                    <ul class="row my-2">
+                                        <li class="col-8 ratfac">
+                                            <i class="bi text-warning bi-star-fill"></i>
+                                            <i class="bi text-warning bi-star-fill"></i>
+                                            <i class="bi text-warning bi-star-fill"></i>
+                                            <i class="bi text-warning bi-star-fill"></i>
+                                            <i class="bi text-warning bi-star-fill"></i>
+                                        </li>
+                                        <li class="col-4"><span class="text-success fs-8 float-end">Detail</span></li>
+                                    </ul>
+                                    <b class="fs-8">Harga : 200.000</b>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+
+
+               <div class="col-md-4 mb-3">
+                    <a href="single.php">
+                          <div class="app-cover p-2 shadow-md bg-white">
+                            <div class="row">
+                                 <div class="img-cover pe-0 col-4"> <img class="rounded" src="assets/images/books/awbook4.jpg" alt="terabytee"></div>
+                                 <div class="det mt-2 col-8">
+                                     <h5 class="mb-0 fs-6">Franchising Jesus<br>&nbsp;</h5>
+                                     <span class="fs-8">Rohani</span>
+                                     <ul class="row my-2">
+                                         <li class="col-8 ratfac">
+                                             <i class="bi text-warning bi-star-fill"></i>
+                                             <i class="bi text-warning bi-star-fill"></i>
+                                             <i class="bi text-warning bi-star-fill"></i>
+                                             <i class="bi text-warning bi-star-fill"></i>
+                                             <i class="bi text-warning bi-star-fill"></i>
+                                         </li>
+                                         <li class="col-4"><span class="text-success fs-8 float-end">Detail</span></li>
+                                     </ul>
+                                     <b class="fs-8">Harga : 200.000</b> 
+                                 </div>
+                            </div>
+                       </div>
+                   </a>
+               </div>
+
+                <div class="col-md-4 mb-3">
+                    <a href="single.php">
+                        <div class="app-cover p-2 shadow-md bg-white">
+                            <div class="row">
+                                <div class="img-cover pe-0 col-4"> <img class="rounded" src="assets/images/books/awbook2.jpg" alt="terabytee"></div>
+                                <div class="det mt-2 col-8">
+                                    <h5 class="mb-0 fs-6">Kebijakan Setengah Hati </h5>
+                                    <span class="fs-8">Industri</span>
+                                    <ul class="row my-2">
+                                        <li class="col-8 ratfac">
+                                            <i class="bi text-warning bi-star-fill"></i>
+                                            <i class="bi text-warning bi-star-fill"></i>
+                                            <i class="bi text-warning bi-star-fill"></i>
+                                            <i class="bi text-warning bi-star-fill"></i>
+                                            <i class="bi text-warning bi-star-fill"></i>
+                                        </li>
+                                        <li class="col-4"><span class="text-success fs-8 float-end">Detail</span></li>
+                                    </ul>
+                                    <b class="fs-8">Harga : 150.000</b>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="col-md-4 mb-3">
+                    <a href="single.php">
+                      <div class="app-cover p-2 shadow-md bg-white">
+                            <div class="row">
+                                 <div class="img-cover pe-0 col-4"> <img class="rounded" src="assets/images/books/awbook3.jpg" alt="terabytee"></div>
+                                 <div class="det mt-2 col-8">
+                                     <h5 class="mb-0 fs-6">Jeritan Energi<br>&nbsp;</h5>
+                                     <span class="fs-8">Industri</span>
+                                     <ul class="row my-2">
+                                         <li class="col-8 ratfac">
+                                             <i class="bi text-warning bi-star-fill"></i>
+                                             <i class="bi text-warning bi-star-fill"></i>
+                                             <i class="bi text-warning bi-star-fill"></i>
+                                             <i class="bi text-warning bi-star-fill"></i>
+                                             <i class="bi text-warning bi-star-fill"></i>
+                                         </li>
+                                         <li class="col-4"><span class="text-success fs-8 float-end">Detail</span></li>
+                                     </ul>
+                                     <b class="fs-8">Harga : 150.000</b>
+                                 </div>
+                            </div>
+                       </div>
+                   </a>
+               </div>
+
                 <div class="col-md-4 mb-3">
                        <a href="single.php">
                           <div class="app-cover p-2 shadow-md bg-white">
@@ -8,7 +110,7 @@
                                  <div class="img-cover pe-0 col-4"> <img class="rounded" src="assets/images/books/awbook1.jpg" alt="terabytee"></div>
                                  <div class="det mt-2 col-8">
                                      <h5 class="mb-0 fs-6">Profil Industri Keramik Indonesia</h5>
-                                     <span class="fs-8">Keramik</span>
+                                     <span class="fs-8">Industri</span>
                                      <ul class="row my-2">
                                          <li class="col-8 ratfac">
                                              <i class="bi text-warning bi-star-fill"></i>
@@ -19,117 +121,17 @@
                                          </li>
                                          <li class="col-4"><span class="text-success fs-8 float-end">Detail</span></li>
                                      </ul>
-                                     <b class="fs-8">Harga : 100.000</b>
+                                     <b class="fs-8">Harga : 160.000</b>
                                  </div>
                             </div>
                        </div>
                     </a>
                </div>
 
-               <div class="col-md-4 mb-3">
-                    <a href="single.php">
-                          <div class="app-cover p-2 shadow-md bg-white">
-                            <div class="row">
-                                 <div class="img-cover pe-0 col-4"> <img class="rounded" src="assets/images/books/awbook2.jpg" alt="terabytee"></div>
-                                 <div class="det mt-2 col-8">
-                                     <h5 class="mb-0 fs-6">Kebijakan Setengah Hati </h5>
-                                     <span class="fs-8">Gula</span>
-                                     <ul class="row my-2">
-                                         <li class="col-8 ratfac">
-                                             <i class="bi text-warning bi-star-fill"></i>
-                                             <i class="bi text-warning bi-star-fill"></i>
-                                             <i class="bi text-warning bi-star-fill"></i>
-                                             <i class="bi text-warning bi-star-fill"></i>
-                                             <i class="bi text-warning bi-star-fill"></i>
-                                         </li>
-                                         <li class="col-4"><span class="text-success fs-8 float-end">Detail</span></li>
-                                     </ul>
-                                     <b class="fs-8">Harga : 100.000</b>
-                                 </div>
-                            </div>
-                       </div>
-                   </a>
-               </div>
 
-
-               <div class="col-md-4 mb-3">
-                    <a href="single.php">
-                      <div class="app-cover p-2 shadow-md bg-white">
-                            <div class="row">
-                                 <div class="img-cover pe-0 col-4"> <img class="rounded" src="assets/images/books/awbook3.jpg" alt="terabytee"></div>
-                                 <div class="det mt-2 col-8">
-                                     <h5 class="mb-0 fs-6">Jeritan Energi</h5>
-                                     <span class="fs-8">Energi</span>
-                                     <ul class="row my-2">
-                                         <li class="col-8 ratfac">
-                                             <i class="bi text-warning bi-star-fill"></i>
-                                             <i class="bi text-warning bi-star-fill"></i>
-                                             <i class="bi text-warning bi-star-fill"></i>
-                                             <i class="bi text-warning bi-star-fill"></i>
-                                             <i class="bi text-warning bi-star-fill"></i>
-                                         </li>
-                                         <li class="col-4"><span class="text-success fs-8 float-end">Detail</span></li>
-                                     </ul>
-                                     <b class="fs-8">Harga : 100.000</b>
-                                 </div>
-                            </div>
-                       </div>
-                   </a>
-               </div>
-
+                
 
                <!-- <div class="col-md-4 mb-3">
-                    <a href="single.php">
-                          <div class="app-cover p-2 shadow-md bg-white">
-                            <div class="row">
-                                 <div class="img-cover pe-0 col-4"> <img class="rounded" src="assets/images/books/4.png" alt="terabytee"></div>
-                                 <div class="det mt-2 col-8">
-                                     <h5 class="mb-0 fs-6">Spider Man</h5>
-                                     <span class="fs-8">Racing</span>
-                                     <ul class="row my-2">
-                                         <li class="col-8 ratfac">
-                                             <i class="bi text-warning bi-star-fill"></i>
-                                             <i class="bi text-warning bi-star-fill"></i>
-                                             <i class="bi text-warning bi-star-fill"></i>
-                                             <i class="bi bi-star-fill"></i>
-                                             <i class="bi bi-star-fill"></i>
-                                         </li>
-                                         <li class="col-4"><span class="text-success fs-8 float-end">Detail</span></li>
-                                     </ul>
-                                     <b class="fs-8">Harga : 18$</b>
-                                 </div>
-                            </div>
-                       </div>
-                   </a>
-               </div>
-
-
-               <div class="col-md-4 mb-3">
-                      <a href="single.php">
-                          <div class="app-cover p-2 shadow-md bg-white">
-                            <div class="row">
-                                 <div class="img-cover pe-0 col-4"> <img class="rounded" src="assets/images/books/5.png" alt="terabytee"></div>
-                                 <div class="det mt-2 col-8">
-                                     <h5 class="mb-0 fs-6">Resident Evil</h5>
-                                     <span class="fs-8">Racing</span>
-                                     <ul class="row my-2">
-                                         <li class="col-8 ratfac">
-                                             <i class="bi text-warning bi-star-fill"></i>
-                                             <i class="bi text-warning bi-star-fill"></i>
-                                             <i class="bi text-warning bi-star-fill"></i>
-                                             <i class="bi bi-star-fill"></i>
-                                             <i class="bi bi-star-fill"></i>
-                                         </li>
-                                         <li class="col-4"><span class="text-success fs-8 float-end">Detail</span></li>
-                                     </ul>
-                                     <b class="fs-8">Harga : 18$</b>
-                                 </div>
-                            </div>
-                       </div>
-                   </a>
-               </div>
-
-               <div class="col-md-4 mb-3">
                     <a href="single.php">
                           <div class="app-cover p-2 shadow-md bg-white">
                             <div class="row">
